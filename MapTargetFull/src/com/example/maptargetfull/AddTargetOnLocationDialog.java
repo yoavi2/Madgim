68a996cd272442f60b263951a1801c741e6bd2e7
sagihatzabi <sagihatzabi@fe80:0:0:0:fcf3:b0ff:fef5:8099%8>
2014-11-09 09:25:33 +0200

    New images

--- MapTargetFull/src/com/example/maptargetfull/AddTargetOnLocationDialog.java
+++ MapTargetFull/src/com/example/maptargetfull/AddTargetOnLocationDialog.java
@@ -20,17 +20,19 @@ public class AddTargetOnLocationDialog extends DialogFragment {
 	private EditText mETName;
 	private double mLatitude;
 	private double mLongitude;
-	
 
-	 public interface AddTargetOnLocationListener {
-		 void createPointOnLocation(String name, GoogleMapFragment.target_type type, double latitude, double longitude);
-	    }
-	
+	public interface AddTargetOnLocationListener {
+		void createPointOnLocation(String name,
+				GoogleMapFragment.target_type type, double latitude,
+				double longitude);
+	}
+
 	public AddTargetOnLocationDialog() {
 		// Empty constructor required for DialogFragment
 	}
 
-	public static AddTargetOnLocationDialog newInstance(String title, double latitude, double longitude,  String callertag) {
+	public static AddTargetOnLocationDialog newInstance(String title,
+			double latitude, double longitude, String callertag) {
 		AddTargetOnLocationDialog frag = new AddTargetOnLocationDialog();
 		Bundle args = new Bundle();
 		args.putString("title", title);
@@ -50,7 +52,7 @@ public class AddTargetOnLocationDialog extends DialogFragment {
 		this.mCallerTag = getArguments().getString("callertag");
 		this.mLatitude = getArguments().getDouble("latitude");
 		this.mLongitude = getArguments().getDouble("longitude");
-		
+
 		this.mETName = (EditText) view.findViewById(R.id.et_name);
 		this.mETName.setOnClickListener(new OnClickListener() {
 			@Override
@@ -58,7 +60,7 @@ public class AddTargetOnLocationDialog extends DialogFragment {
 				mETName.setCompoundDrawables(null, null, null, null);
 			}
 		});
-		
+
 		((Button) view.findViewById(R.id.btn_accept))
 				.setOnClickListener(new View.OnClickListener() {
 					@Override
@@ -79,14 +81,18 @@ public class AddTargetOnLocationDialog extends DialogFragment {
 									null);
 						} else {
 							GoogleMapFragment.target_type tt_type = GoogleMapFragment.target_type.FRIEND;
-							if (((RadioGroup) getView().findViewById(R.id.rg_target_type))
+							if (((RadioGroup) getView().findViewById(
+									R.id.rg_target_type))
 									.getCheckedRadioButtonId() == R.id.rb_enemy) {
 								tt_type = GoogleMapFragment.target_type.ENEMY;
 							}
-							
-							 AddTargetOnLocationListener fragment = (AddTargetOnLocationListener) getActivity().getFragmentManager().findFragmentByTag(mCallerTag);
-					            fragment.createPointOnLocation(mETName.getText().toString(), tt_type, mLatitude, mLongitude);
-					            		
+
+							AddTargetOnLocationListener fragment = (AddTargetOnLocationListener) getActivity()
+									.getFragmentManager().findFragmentByTag(
+											mCallerTag);
+							fragment.createPointOnLocation(mETName.getText()
+									.toString(), tt_type, mLatitude, mLongitude);
+
 							dismiss();
 						}
 					}
