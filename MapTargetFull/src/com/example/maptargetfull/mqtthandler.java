10a0a5460a7c0f23f34df28ae02834785aad7648
sylar2290 <mrpoll07@gmail.com>
2014-11-12 21:05:48 +0200

    Add methods to pointsDB

--- MapTargetFull/src/com/example/maptargetfull/mqtthandler.java
+++ MapTargetFull/src/com/example/maptargetfull/mqtthandler.java
@@ -1,16 +1,18 @@
 package com.example.maptargetfull;
 
+import java.util.ArrayList;
+
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
 import android.widget.Toast;
 
 import com.example.maptargetfull.GlobalParams.markerType;
+import com.example.maptargetfull.PointsDBAccess.Point;
 import com.example.maptargetfull.PointsDBAccess.PointForSync;
 import com.example.maptargetfull.SQLiteDB.Points;
 
@@ -52,13 +54,12 @@ public class mqtthandler implements MqttCallback
 //		.setContentTitle("Mqtt")
 //		.setContentText("Message has arrived!");
 //		m.notify(123, b.build());
+		JSONObject json = null;
 		
 		if (topic.equals("insert"))
-		{
-			JSONObject json = null;
+		{	
 			try {
-				JSONArray a = new JSONArray(message.toString());
-				json = a.getJSONObject(0);
+				json = new JSONObject(message.toString());
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
@@ -82,7 +83,7 @@ public class mqtthandler implements MqttCallback
 					Points.Columns.point_type);
 			long rowID = pointsDB.createPoint(
 					ps.first_name,
-					ps.last_name,
+		 			ps.last_name,
 					ps.longitude,
 					ps.langitude,
 					ps.is_google == 1 ? true : false,
@@ -94,6 +95,49 @@ public class mqtthandler implements MqttCallback
 							: markerType.Truck, ps.langitude,
 					ps.longitude);
 			Toast.makeText(this.context, ps.first_name + " added!", Toast.LENGTH_SHORT).show();
+		}
+		else if (topic.equals("update")) 
+		{
+			try {
+				json = new JSONObject(message.toString());
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+			PointsDBAccess pointsDB = new PointsDBAccess(this.context);
+			PointForSync ps = pointsDB.new PointForSync();
+			ps.first_name = json.getString(
+					Points.Columns.first_name);
+			ps.last_name = json.getString(
+					Points.Columns.last_name);
+			ps.longitude = json.getDouble(
+					Points.Columns.longitude);
+			ps.langitude = json.getDouble(
+					Points.Columns.langitude);
+			ps.is_google = json.getInt(
+					Points.Columns.is_google);
+			ps.server_id = json.getString(
+					"_id");
+			ps.pointType = json.getInt(
+					Points.Columns.point_type);
+		    ArrayList<Point> arr = pointsDB.getPoints(ps.is_google == 1 ? true : false);
+		    for (int i = 0; i < arr.size(); i++) {
+		    	
+			}
+			long rowID = pointsDB.createPoint(
+					ps.first_name,
+		 			ps.last_name,
+					ps.longitude,
+					ps.langitude,
+					ps.is_google == 1 ? true : false,
+				    ps.pointType);
+			pointsDB.SetServerID(rowID, ps.server_id);
+			pointsDB.SetSynched(rowID);
+			GlobalParams.getInstance().mCurrMap.addMarkerOnLocationOffline(ps.first_name,
+					ps.pointType == 1 ? markerType.Tank
+							: markerType.Truck, ps.langitude,
+					ps.longitude);
 		}
 	}
 
