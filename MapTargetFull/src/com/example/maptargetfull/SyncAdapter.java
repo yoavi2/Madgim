bba1e29e77d5f0694f7a48a7f2216fbff3f1c8aa
sylar2290 <mrpoll07@gmail.com>
2014-11-17 14:01:37 +0200

    IP

--- MapTargetFull/src/com/example/maptargetfull/SyncAdapter.java
+++ MapTargetFull/src/com/example/maptargetfull/SyncAdapter.java
@@ -38,7 +38,7 @@ import com.example.maptargetfull.PointsDBAccess.PointForSync;
 import com.example.maptargetfull.SQLiteDB.Points;
 
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
-	private final String url = "http://192.168.43.69:3000/friends";
+	private final String url = "http://192.168.43.48:3000/friends";
 	
 	public SyncAdapter(Context context, boolean autoInitialize) {
 		super(context, autoInitialize);
